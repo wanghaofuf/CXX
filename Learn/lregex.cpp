e5fc6d3c66d3772b6c0ed16cae35b4976308a681
root <root@localhost.localdomain>
2020-10-14 10:53:36 +0800

    regex_replace 是全部替换，不是就替换第一个

--- Learn/lregex.cpp
+++ Learn/lregex.cpp
@@ -41,17 +41,28 @@ void testregex_search(){
 
     std::string str = "wanghaofu is a rich man!";
     std::smatch sm;
-    if(std::regex_search(str,sm,std::regex("fu"))){
+    if(std::regex_search(str,sm,std::regex("(fu)(.*)\\b(ri.*)"))){
         std::cout << sm.prefix().str() << std::endl;
         for(auto x:sm) std::cout << x << std::endl;
         std::cout << sm.suffix().str() << std::endl;
     }
+    std::cmatch cm;
+    if(std::regex_search("there is a subsequence in the string\n",cm,std::regex("\\b(sub)([^ ]*)"))){
+        for(auto x: cm) std::cout << x << std::endl;
+
+    }
 }
+
+void testregex_replace(){
+    std::cout << std::regex_replace("wanghao is rich man,wanghao is good!",std::regex("\\b(wanghao) \\b(is)"),"$1fu is too") << std::endl;
+}
+
 
 int main(int argc, char **argv){
 
     //testregex_match();
     //testmarchresults();
-    testregex_search();
+    //testregex_search();
+    testregex_replace();
     return 0;
 }
